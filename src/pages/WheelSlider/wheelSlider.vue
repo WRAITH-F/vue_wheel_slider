2c4958f0d986ee5412fd452448614d69f36d04ec
wxb <1213422461@qq.com>
2019-06-26 15:26:02 +0800

    懒加载图片移动

--- src/pages/WheelSlider/wheelSlider.vue
+++ src/pages/WheelSlider/wheelSlider.vue
@@ -2,7 +2,7 @@
 	<div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:980px;height:600px;visibility:hidden;overflow: hidden;" :style="{width: wheelWidth+'px',height:wheelHeight+'px'}">
 		<!-- Loading Screen -->
 		<div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
-			<img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="../../assets/spin.svg" />
+			<img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="./spin.svg" />
 		</div>
 		<div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;overflow:hidden;"  :style="{width: wheelWidth+'px',height:wheelHeight+'px'}">
 			<a v-for="wp in wheelPic">
